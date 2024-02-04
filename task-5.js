583b1c77313bd3740e77a9929b4896efa6d4e00a
hammadsadi <hammad.sadi@yahoo.com>
2024-02-04 11:38:17 +0600

    Task 5 done

---
+++ task-5.js
@@ -0,0 +1,6 @@
+function genarateRandomNumber() {
+  let randomGenarate = Math.floor(Math.random() * 10) + 11;
+  return randomGenarate;
+}
+let result = genarateRandomNumber();
+console.log(result);
